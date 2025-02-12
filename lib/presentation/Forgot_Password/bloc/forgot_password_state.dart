dc039f5871b637a3f5307f18ca05668ee44f38d5
HauDuongGL <dphau29082002@gmail.com>
2025-02-12 15:33:35 +0700

    Bloc Register and ForgotPassword

--- lib/presentation/Forgot_Password/bloc/forgot_password_state.dart
+++ lib/presentation/Forgot_Password/bloc/forgot_password_state.dart
@@ -2,5 +2,9 @@ part of 'forgot_password_bloc.dart';
 
 @freezed
 class ForgotPasswordState extends BaseState with _$ForgotPasswordState {
-  const factory ForgotPasswordState.initial() = _Initial;
+  const factory ForgotPasswordState(
+    bool isLoading,
+    String errorMessgae,
+    bool isSuccess,
+  ) = _ForgotPasswordState;
 }
--- lib/presentation/register/bloc/register_bloc.dart
+++ lib/presentation/register/bloc/register_bloc.dart
@@ -1,14 +1,47 @@
+import 'dart:async';
+
 import 'package:flutter_base_bloc/core/base/bloc/base_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter_base_bloc/domain/entities/register.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 part 'register_event.dart';
 part 'register_state.dart';
 part 'register_bloc.freezed.dart';
 
 class RegisterBloc extends BaseBloc<RegisterEvent, RegisterState> {
-  RegisterBloc() : super(_Initial()) {
-    on<RegisterEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  RegisterBloc() : super(const RegisterState(false, '', false)) {
+    on<_Register>(_onRegister);
+  }
+
+  FutureOr<void> _onRegister(
+      _Register event, Emitter<RegisterState> emit) async {
+    emit(state.copyWith(isLoading: true, errorMessage: '', isSuccess: false));
+
+    if (event.registerModel.password != event.registerModel.confirmPassword) {
+      emit(state.copyWith(
+          isLoading: false, errorMessage: "Mật khẩu xác nhận không khớp"));
+      return;
+    }
+
+    try {
+      final supabase = Supabase.instance.client;
+
+      final response = await supabase.auth.signUp(
+        email: event.registerModel.email,
+        password: event.registerModel.password ?? '',
+        data: {'username': event.registerModel.username},
+      );
+
+      if (response.user != null) {
+        emit(state.copyWith(isLoading: false, isSuccess: true));
+      } else {
+        emit(state.copyWith(
+            isLoading: false, errorMessage: "Đăng ký thất bại, thử lại sau"));
+      }
+    } catch (e) {
+      emit(state.copyWith(isLoading: false, errorMessage: e.toString()));
+    }
   }
 }
